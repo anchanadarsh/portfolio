546a6e3dfa659295d838f9fd9668c03fd51f3253
adarsh anchan <anchanadarsh@gmail.com>
2017-09-29 14:38:23 +0530

    index

--- index.php
+++ index.php
@@ -63,6 +63,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
     <script src="https://use.fontawesome.com/e49acd8124.js"></script>
+    
+    
 
     <script>
         angular.module("portfolio", []).controller("portfolioCtrl", function ($scope) {
